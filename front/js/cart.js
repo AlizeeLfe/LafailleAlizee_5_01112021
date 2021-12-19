27ccec451b61ba102d12907edcbd8219ba7e7cd1
Lafaille Alizee <alizeelcast@yahoo.fr>
2021-12-19 23:08:10 +0100

    commentaires ajoutés

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,12 +1,12 @@
-//****************************AFFICHAGE DES PROUITS DANS LE PANIER******************
+//**********************************AFFICHAGE DES PROUITS DANS LE PANIER**********************************
 const cartContainer = document.querySelector(".cart-all-items");
 const cartPrice = document.querySelector(".cart__price");
 const cartOrder = document.querySelector(".cart__order");
 const errorMsg = document.querySelector("#cart__items");
 
-// Recuperation des données contenues dans le LS et conversion en format JSON
+// Recuperation des données du LS avec méthode "getItem", et conversion en format JSON
 let cart = JSON.parse(localStorage.getItem("cart"));
-// Si le panier est vide, afficher un message, cacher le reste de la page
+// Si le panier est vide : afficher un message, et cacher le reste de la page
 if (cart === null) {
   errorMsg.innerHTML = `
     <div class="cart__empty">
@@ -16,9 +16,10 @@ if (cart === null) {
   cartPrice.style.display = "none";
   cartOrder.style.display = "none";
 } else {
+  //Si le panier n'est pas vide : Afficher les produits qui sont dans le local storage
   renderHTML();
-
-  //*************************Gestion du bouton supprimer l'article****************************
+  // GESTION DE LA MODIFICATION DU CONTENU DU PANIER ET DU LS SIMULATANEMENT
+  // Supprimer un article
   let deleteBtn = document.querySelectorAll(".deleteItem");
   deleteBtn.forEach((btn) => {
     btn.addEventListener("click", function (e) {
@@ -34,7 +35,7 @@ if (cart === null) {
     }
     window.location.reload();
   }
-  //*************************Gestion du bouton changement quantité panier****************************
+  // Changer la quantité depuis le panier
   let updateQuantityInput = document.querySelectorAll(".item__quantity");
   updateQuantityInput.forEach((input) => {
     input.addEventListener("change", function (e) {
@@ -48,9 +49,9 @@ if (cart === null) {
       updateTotal();
     }
   }
-  //*******************Gestions Mise à jour quantité totale et prix total*******************
+  // AFFICHER LA "QUANTITÉE TOTALE" ET "PRIX TOTAL" À JOUR
   function updateTotal() {
-    // Récupération de la quantitée totales
+    // Récupération de la quantitée totale
     let totalItemQty = 0;
     cart.forEach((e) => {
       totalItemQty += e.qty;
@@ -69,138 +70,121 @@ if (cart === null) {
   updateTotal();
 }
 
-//***********************************FORMULAIRE****************************
-// Envoi des données au serveur quand on clique sur le bouton commander
+//**********************************FORMULAIRE**********************************
+// ENVOI DES DONNÉES "CONTACT" ET "PRODUCTS" (du LS) AU SERVEUR SI LES DONNÉES SONT OK
+
+// SOUMETTRE LE FORMULAIRE (lorsqu'on clique sur le bouton "commander")
 const sendForm = function () {
-  const btnSendForm = document.querySelector("#order");
-  btnSendForm.addEventListener("click", (e) => {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    // Stopper le comportement par défaut du bouton pour eviter le rechargement de la page (et perdre les infos)
     e.preventDefault();
-    // Récuperation des valeurs du formulaire
-    const contact = {
-      firstName: document.querySelector("#firstName").value,
-      lastName: document.querySelector("#lastName").value,
-      address: document.querySelector("#address").value,
-      city: document.querySelector("#city").value,
-      email: document.querySelector("#email").value,
-    };
-    //Verifications des données
-    let form = document.querySelector(".cart__order__form");
+    // SI LES INFORMATIONS DU FORMULAIRE SONT CONFORMES AUX REGEX ...
+    if (validForm()) {
+      //... alors on récupère ces informations dans un objet "contact" (requested by controllers)
+      const contact = {
+        firstName: document.querySelector("#firstName").value,
+        lastName: document.querySelector("#lastName").value,
+        address: document.querySelector("#address").value,
+        city: document.querySelector("#city").value,
+        email: document.querySelector("#email").value,
+      };
+      //... on construction d'un tableau depuis le local storage et y ajouter les ID (requested by controllers)
+      let idProducts = [];
+      for (let i = 0; i < cart.length; i++) {
+        idProducts.push(cart[i]._id);
+      }
+      //... on met les valeurs du formulaire et les produits sélectionnés dans un objet à envoyer vers le serveur
+      const toSend = {
+        products: idProducts,
+        contact,
+      };
+      //... on envoi l'objet "toSend" avec une requête de type "POST" pour envoyer des données VERS le serveur
+      const promiseCart = {
+        method: "POST",
+        // Prevenir le service web qu'il va recevoir du JSON
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        // Transformer l'objet JavaScript en JSON
+        body: JSON.stringify(toSend),
+      };
 
-    // Controle du Prenom
-    function validFirstName() {
-      const controlFirstName = contact.firstName;
-      if (/^[a-zA-Z-]{2,20}$/.test(controlFirstName)) {
-        return true;
-      } else {
-        let firstNameErrorMsg = document.getElementById("firstNameErrorMsg");
-        firstNameErrorMsg.innerText =
-          "Le prénom ne doit pas contenir de chiffre ou de caractère spécial";
-      }
+      //... on récupère les données de L'API avec en paramètre de requête : un objet init
+      fetch("http://localhost:3000/api/products/order", promiseCart)
+        .then((response) => response.json())
+        // on renvoi les données traitées par JSON
+        .then((data) => {
+          // On vide le LS pour pas que les articles s'affichent encore dans le panier après avoir passer commande
+          localStorage.clear();
+          // On va créer un élément dans le LS "orderId" avec comme valeur les données de la commande a id unique
+          localStorage.setItem("orderId", data.orderId);
+          // On renvoi le client à la page de confirmation
+          document.location.href = "confirmation.html";
+        })
+        .catch((err) => console.log("erreur :" + err));
     }
-
-    // Controle du nom
-    function validLastName() {
-      const controlName = contact.lastName;
-      if (/^[a-zA-Z-]{2,20}$/.test(controlName)) {
-        return true;
-      } else {
-        let lastNameErrorMsg = document.getElementById("lastNameErrorMsg");
-        lastNameErrorMsg.innerText =
-          "Le nom ne doit pas contenir de chiffre ou de caractère spécial";
-      }
-    }
-    // Controle de l'adresse
-    function validAddress() {
-      const controlAddress = contact.address;
-      if (
-        /^[0-9]{1,3}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)+/.test(controlAddress)) {
-        return true;
-      } else {
-        let addressErrorMsg = document.getElementById("addressErrorMsg");
-        addressErrorMsg.innerText =
-          "L'adresse indiquée ne correspond pas au format français";
-      }
-    }
-    // Controle de la ville
-    function validCity() {
-      const controlCity = contact.city;
-      if (/^[a-zA-Z-']{2,20}$/.test(controlCity)) {
-        return true;
-      } else {
-        let cityErrorMsg = document.getElementById("cityErrorMsg");
-        cityErrorMsg.innerText =
-          "La ville ne doit pas contenir de chiffre ou de caractère spécial";
-      }
-    }
-    // Controle de l'email
-    function validEmail() {
-      const controlEmail = contact.email;
-      if (/^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/.test(controlEmail)) {
-        return true;
-      } else {
-        let emailErrorMsg = document.getElementById("emailErrorMsg");
-        emailErrorMsg.innerText = "Le format de l'adresse mail est incorrect";
-      }
-    }
-
-    //*******************************************Recuperation données formulaire****************************************
-    //Construction d'un array depuis le local storage
-    let idProducts = [];
-    for (let i = 0; i < cart.length; i++) {
-      idProducts.push(cart[i]._id);
-    }
-    // Mettre l'objet "contact" dans le LS si les values sont présentes dans les champs
-    function validForm() {
-      if (
-        validFirstName(form.firstName) &&
-        validLastName(form.lastName) &&
-        validAddress(form.address) &&
-        validCity(form.city) &&
-        validEmail(form.email)
-      ) {
-        localStorage.setItem("contact", JSON.stringify(contact));
-        return true;
-        //   form.submit();
-      } else {
-        alert("Un ou plusieurs champs du formulaire est mal renseigné");
-      }
-    }
-    validForm();
-    // Mettre les values du formulaire et mettre les produits sélectionnés dans un objet à envoyer vers le serveur
-    const toSend = {
-      products: idProducts,
-      contact,
-    };
-    // Evoi de l'objet "toSend" vers le serveur
-    const promiseCart = {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(toSend),
-    };
-
-    console.log(promiseCart);
-
-    fetch("http://localhost:3000/api/products/order", promiseCart)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        localStorage.setItem("orderId", data.orderId);
-        if (validForm()) {
-          document.location.href = "confirmation.html?id=" + data.orderId;
-        }
-      })
-      .catch((err) => console.log("erreur :" + err));
   });
 };
 sendForm();
 
+// FONCTION POUR VÉRIFIER LA CONFORMITÉ DE L'ENSEMBLE DES VALEURS ENTRÉES DANS LE FORMULAIRE
+function validForm() {
+  const input = {
+    firstName: document.querySelector("#firstName"),
+    lastName: document.querySelector("#lastName"),
+    address: document.querySelector("#address"),
+    city: document.querySelector("#city"),
+    email: document.querySelector("#email"),
+  };
+  if (
+    checkRegex(
+      input.firstName,
+      "^[a-zA-Z-]{2,20}$",
+      "Le prénom ne doit pas contenir de chiffre ou de caractère spécial"
+    ) &&
+    checkRegex(
+      input.lastName,
+      "^[a-zA-Z-]{2,20}$",
+      "Le nom ne doit pas contenir de chiffre ou de caractère spécial"
+    ) &&
+    checkRegex(
+      input.address,
+      "^[0-9]{1,3}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)+",
+      "L'adresse comporte un caractère non autorisé"
+    ) &&
+    checkRegex(
+      input.city,
+      "^[a-zA-Z-']{2,20}$",
+      "La ville ne doit pas contenir de chiffre ou de caractère spécial"
+    ) &&
+    checkRegex(
+      input.email,
+      "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$",
+      "Le format de l'adresse mail est incorrect"
+    )
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// FONCTION POUR VÉRIFIER QUE LA VALEUR DE L'INPUT EST CONFORME A CE QUI EST DEMANDÉ GRACE AUX TEST REGEX
+function checkRegex(element, regex, message) {
+  // Opérateur "new" pour créer un nouvel objet qui teste la valeur de l'input avec la méthode "RegExp.test()"
+  if (new RegExp(regex).test(element.value)) {
+    return true;
+  } else {
+    // Si valeur de l'input non conforme : on affiche le message d'erreur
+    element.nextElementSibling.innerText = message;
+    return false;
+  }
+}
 
+// FONCTION QUI PERMET D'AFFICHER CHAQUE PRODUIT DANS LE PANIER
 function renderHTML() {
   let html = "";
-  //Si le panier n'est pas vide, il faut afficher les produits qui sont dans le local storage
   cart.forEach((product, index) => {
     html += `     <article class="cart__item" data-id="${product._id}">
                     <div class="cart__item__img">
