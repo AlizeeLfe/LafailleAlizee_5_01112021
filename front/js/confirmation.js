16882aff9db4cdcac5ff92a8a6785ec10905aac5
Lafaille Alizee <alizeelcast@yahoo.fr>
2021-12-19 23:08:42 +0100

    commentaires ajoutés

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,6 +1,6 @@
-function checkout(){
-    const orderId = document.getElementById('orderId');
-    orderId.innerHTML = localStorage.getItem('orderId');
-    localStorage.clear();
+function checkout() {
+  const orderId = document.getElementById("orderId");
+  orderId.innerHTML = localStorage.getItem("orderId");
+  localStorage.clear();
 }
 checkout();
